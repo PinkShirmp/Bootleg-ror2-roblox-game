2ad97d079b59efe1847c3fafc7557ca1094540da
NGUYEN MINH TUE <nminhtue094@gmail.com>
2023-06-02 14:37:19 +0700

    Worked on data

--- src/server/Sources/Server.server.lua
+++ src/server/Sources/Server.server.lua
@@ -1,3 +1,4 @@
+local HttpService = game:GetService("HttpService")
 local Players=game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 Players.CharacterAutoLoads =false
@@ -8,34 +9,33 @@ local CharacterSelector = ReplicatedStorage:WaitForChild"CharacterSelector"
 
 local Char_Pos =workspace:WaitForChild"Pos"
 
+
+
 local DataKey ={
     "Character",
     "Coins"
 }
+
 local DataDefault = {
     ["Character"]="Bandit",
     ["Coins"]=0,
 }
 
+
+
 for i , v in ipairs(DataKey) do
     Datastore2.Combine("Data",v)
 end
 
-Players.PlayerAdded:Connect(function(player)
-    _G[player.UserId]={}
-    _G[player.UserId]["Data"]={}
-    for i , keys in ipairs(DataKey) do
-        _G[player.UserId][keys]=Datastore2(keys,player)
-        _G[player.UserId]["Data"]=_G[player.UserId][keys]:Get(DataDefault[keys])
-    end
-    local function OnUpdate() 
-        --le funny update
-    end
-    for i , v in ipairs(DataKey) do
-        _G[player.UserId][v]:OnUpdate(OnUpdate)
-    end
-    --Load Selected Character
-    task.delay(2,function()
+
+local Data=Instance.new"Folder"
+Data.Parent =ReplicatedStorage
+Data.Name = "PlayerDatas"
+
+
+function LoadCharacter(player)
+     --Load Selected Character
+     task.delay(2,function()
         print(_G[player.UserId])
         local Sel_Char = CharacterSelector:FindFirstChild(_G[player.UserId][DataKey[1]]:Get())
         print(Sel_Char)
@@ -45,16 +45,57 @@ Players.PlayerAdded:Connect(function(player)
             cloned_.PrimaryPart.CFrame = Char_Pos.CFrame
             local AnimControl,Animations = cloned_:FindFirstChildWhichIsA("AnimationController"),cloned_:WaitForChild("Animations")
             if AnimControl and Animations then
-                AnimControl:LoadAnimation(Animations:WaitForChild("Selector")):Play(0)
+                local AppearAnimation=AnimControl:LoadAnimation(Animations:WaitForChild("Selector"))
+                local IdleAnim = AnimControl:LoadAnimation(Animations:WaitForChild("Idle"))
+                AppearAnimation:Play(0)
+                local net
+                net = AppearAnimation:GetMarkerReachedSignal("Stop"):Connect(function()
+                    print("REACHED")
+                    AppearAnimation:Stop(0)
+                    IdleAnim:Play(0)
+                    net:Disconnect()  
+                end)
             end
         else
             warn("Can't not find character")
             player:Kick("Data Error")
         end
     end)
+end
+
+
+
+Players.PlayerAdded:Connect(function(player)
+    _G[player.UserId]={}
+    _G[player.UserId]["Data"]={}
+    local PlayerData = Instance.new"Folder"
+    PlayerData.Parent = Data
+    PlayerData.Name = player.Name
+
+    local DataDisplay = Instance.new"StringValue"
+    DataDisplay.Name ="Data"
+    DataDisplay.Parent = PlayerData
+
+    local DataToEnCode = {}
+    for i , keys in ipairs(DataKey) do
+        _G[player.UserId][keys]=Datastore2(keys,player)
+        DataToEnCode[keys]= _G[player.UserId][keys]:Get()
+    end
+    local json = HttpService:JSONEncode(DataToEnCode)
+    DataDisplay.Value =json
+    local function OnUpdate(newValue)
+        table.clear(DataToEnCode)
+        for i , keys in ipairs(DataKey) do
+            _G[player.UserId][keys]=Datastore2(keys,player)
+            DataToEnCode[keys]= _G[player.UserId][keys]:Get()
+        end
+        DataDisplay.Value = HttpService:JSONEncode(DataToEnCode)
+    end
+    for i , v in ipairs(DataKey) do
+        _G[player.UserId][v]:OnUpdate(OnUpdate)
+    end
 end)
 
 Players.PlayerRemoving:Connect(function(player)
-    _G[player.UserId]["Data"]=nil
     _G[player.UserId]=nil    
 end)
